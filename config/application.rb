1d9a47dd37711472b0b4ca4a24b0291573abd37c
Wilson Anjos <wilsonanjos_@live.com>
2023-06-27 15:40:51 -0300

    configuration timezone

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,19 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+def set_location_configs_and_timezone
+  config.time_zone = 'Brasilia'
+  config.i18n.load_path += Dir[Rails.root.join('config','locales', '**/*.{rb,yml}').to_s]
+  config.i18n.available_locales = ['pt-BR']
+  config.i18n.default_locale = 'pt-BR'
+end
+
 module JobOpportunity
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    set_location_configs_and_timezone
 
     # Configuration for the application, engines, and railties goes here.
     #
